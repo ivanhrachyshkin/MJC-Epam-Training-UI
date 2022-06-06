19d3d2753d25705e46d2774f6f87b1781d579f51
Ivan Hrachyshkin <ivanhrachyshkin@gmail.com>
2022-06-06 22:24:32 +0300

    hover added

--- public/js/categories_images.js
+++ public/js/categories_images.js
@@ -7,8 +7,11 @@ async function getData() {
 
     let temp = "";
     for (let i = 0; i < length; i++) {
-        temp += `<div class="category_image">
-            <img src="http://127.0.0.1:8887/categories/${data.content[i].image}"  width="90" height="90"/>
+        temp += `<div class="category_image" id="category_image">
+            <img class="image" src="http://127.0.0.1:8887/categories/${data.content[i].image}"/>
+            <div class="image-overlay" id="image-overlay">
+            <div class="image-overlay-text">${data.content[i].name}</div>
+            </div>
         </div>`;
 
     }
